04d7961f1a2d23f11026b8ae53187a0fad2dfbc6
Loren Epler <LorenEpler@gmail.com>
2017-02-15 19:55:43 -0600

    Added permalinks to main page, so user can click title to be taken to an individual blog posting

--- main.py
+++ main.py
@@ -35,16 +35,16 @@ class Handler(webapp2.RequestHandler):
     def render(self, template, **kw):
         self.write(self.render_str(template, **kw))
 
-class Art(db.Model):
+class Posting(db.Model):
     title = db.StringProperty(required = True)
-    art = db.TextProperty(required = True)
+    posting = db.TextProperty(required = True)
     created = db.DateTimeProperty(auto_now_add = True)
 
 class MainPage(Handler):
-    def render_front(self, title="", art=""):
-        arts = db.GqlQuery("SELECT * FROM Art ORDER BY created DESC LIMIT 5")
+    def render_front(self, title="", posting=""):
+        postings = db.GqlQuery("SELECT * FROM Posting ORDER BY created DESC LIMIT 5")
 
-        self.render("front.html", title=title, art=art, arts=arts)
+        self.render("front.html", title=title, posting=posting, postings=postings)
 
     def get(self):
         self.render_front()
@@ -55,21 +55,25 @@ class NewPostPage(Handler):
 
     def post(self):
         title = self.request.get("title")
-        art = self.request.get("art")
+        posting = self.request.get("posting")
 
-        if title and art:
-            a = Art(title = title, art = art)
+        if title and posting:
+            a = Posting(title = title, posting = posting)
             a.put()
 
             self.redirect("/")
         else:
             error = "Please enter a title and a body to your blog post."
-            ##self.render_front(title, art, error)
-            self.render("newpost.html", title=title, art=art, error=error)
+            ##self.render_front(title, posting, error)
+            self.render("newpost.html", title=title, posting=posting, error=error)
 
 class ViewPostHandler(webapp2.RequestHandler):
     def get(self, id):
-        pass #replace this with some code to handle the request
+    #    self.response.write(id)
+
+#    def post(self):
+        blog_post = Posting.get_by_id( int(id))
+        self.response.write("<h2>" + blog_post.title + "</h2>" + "<br>" + blog_post.posting)
 
 app = webapp2.WSGIApplication([
     ('/', MainPage),
